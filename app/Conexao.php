b4301d780c930d60bc145fefb37ceb9c5362eb05
alvaro220592 <alvaro220592@gmail.com>
2022-03-02 22:40:18 -0300

    ...

--- app/Conexao.php
+++ app/Conexao.php
@@ -7,7 +7,8 @@ class Conexao{
 
     public static function getConn(){
         if(!isset(self::$instance)){
-            $pdo = new \PDO();
+            self::$instance = new \PDO('mysql:host=localhost; dbname=php_pdf; charset=utf8', 'estudo', '');
+            return self::$instance;
         }
     }
 }
